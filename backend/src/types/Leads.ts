0e1514a43dd40ddf17ba9f7c0fae0f1c07d0f4d8
Pedro Machado <pedro.machado@five9.com>
2025-12-06 15:53:47 +0000

    WIP: add a type for the request body to find phones

---
+++ backend/src/types/Leads.ts
@@ -0,0 +1,13 @@
+export type LeadFindPhoneReqBody = {
+  id?: string
+  createdAt?: string
+  updatedAt?: string
+  firstName?: string
+  lastName?: string
+  email?: string
+  jobTitle?: string
+  countryCode?: string
+  companyName?: string
+  message?: string
+  emailVerified?: boolean
+}
